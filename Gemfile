4900c2431923bbe5e38df3c2f1661d88f4a24786
Your Name <avchyanatur@gmail.com>
2017-04-06 16:13:16 +0300

    add form

--- Gemfile
+++ Gemfile
@@ -6,38 +6,31 @@ git_source(:github) do |repo_name|
 end
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
-gem 'pg'            , group: :production
-gem 'rails_12factor', group: :production
-
-
+gem 'rails', '~> 5.0.2'
+gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
-
-gem 'coffee-rails', '~> 4.2'
 gem 'sass-rails', '~> 5.0'
+gem 'coffee-rails', '~> 4.2'
 gem 'haml-rails'
-
-gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
-gem 'jquery-ui-rails', '~> 5.0.5'
 
 gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'omniauth-vkontakte'
 
-gem 'jbuilder', '~> 2.5'
-
-group :development, :test do
+group :development do
   gem 'byebug', platform: :mri
   gem 'mysql2', '>= 0.3.18', '< 0.5'
 
   gem 'web-console', '>= 3.3.0'
-
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'rails_12factor', group: :production
 
 ruby "2.3.1"
---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,61 @@
+// // Blurred background
+// var CanvasImage = function (e, t) {
+//   this.image = t,
+//   this.element = e,
+//   this.element.width = this.image.width,
+//   this.element.height = this.image.height;
+//   var n = navigator.userAgent.toLowerCase().indexOf("chrome") > -1,
+//       r = navigator.appVersion.indexOf("Mac") > -1;
+//   n && r && (this.element.width = Math.min(this.element.width, 1680), this.element.height = Math.min(this.element.height, 1050)),
+//   this.context = this.element.getContext("2d"),
+//   this.context.drawImage(this.image, 0, 0)
+// };
+// CanvasImage.prototype = {
+//   blur: function (e) {
+//     this.context.globalAlpha = .5;
+//     for (var t = -e; t <= e; t += 2)
+//     for (var n = -e; n <= e; n += 2)
+//     this.context.drawImage(this.element, n, t),
+//     n >= 0 && t >= 0 && this.context.drawImage(this.element, -(n - 1), -(t - 1));
+//     this.context.globalAlpha = 1
+//   }
+// },
+
+// $(function () {
+//   var image, canvasImage, canvas;
+//   $(".blur").each(function () {
+//     canvas = this,
+//     image = new Image,
+//     image.onload = function () {
+//       canvasImage = new CanvasImage(canvas, this),
+//       canvasImage.blur(4)
+//     },
+//     image.src = $(this).attr("src");
+//   });
+// });
+
+
+// // Loader
+// var loader = document.getElementById('loader')
+//   , border = document.getElementById('border')
+//   , α = 0
+//   , π = Math.PI
+//   , t = 180;
+
+// (function draw() {
+//   α++;
+//   α %= 360;
+//   var r = ( α * π / 180 )
+//     , x = Math.sin( r ) * 125
+//     , y = Math.cos( r ) * - 125
+//     , mid = ( α > 180 ) ? 1 : 0
+//     , anim = 'M 0 0 v -125 A 125 125 1 '
+//            + mid + ' 1 '
+//            +  x  + ' '
+//            +  y  + ' z';
+
+//   loader.setAttribute( 'd', anim );
+//   border.setAttribute( 'd', anim );
+
+//   setTimeout(draw, t); // Redraw
+// })();
