142c4b7aedcde01c5014ca79c775d39427639182
Your Name <avchyanatur@gmail.com>
2017-04-07 01:08:23 +0300

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   namespace :users do
-    get 'omniauth_callbacks/facebook'
-  end
-
-  namespace :users do
     get 'omniauth_callbacks/vkontakte'
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, :only => [:index, :destroy]
+  resources :users#, :only => [:index, :destroy, :new]
   root 'users#index'
 end
