3f7e3e1e073c45d04afdd39b9a111c5517e6a858
Atur <avchyanatur@gmail.com>
2017-11-14 02:03:04 +0400

    vk email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # url: https://vk.com/editapp?id=5967554&section=options
-  config.omniauth :vkontakte, '5984475', '8Fli51GbJc430pjKjbgt'#, scope: 'user, public_repo, email, phone'#, provider_ignores_state: true#, scope: "email"
+  config.omniauth :vkontakte, '5984475', '8Fli51GbJc430pjKjbgt', scope: 'email'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
